01bfb9165a86da117c1055965ada98691528a6da
Northengard <dmitr.gorbunow2011@yandex.ru>
2020-03-17 19:37:09 +0300

    add face bounding box scaling factor, docstrings

--- data/processing/videos_to_frames.py
+++ data/processing/videos_to_frames.py
@@ -26,6 +26,8 @@ from utils.storage import TqdmToLogger, get_logger
 
 VID_NUM_FRAMES = 300
 FPS = 30
+IMAGE_WIDTH = 1920
+IMAGE_HEIGHT = 1080
 LOG_DIR = list(map(lambda x: os.path.basename(x) == 'dl_pipe', os.sys.path)).index(True)
 LOG_DIR = os.path.join(os.sys.path[LOG_DIR], 'logs', 'data_processing')
 
@@ -35,15 +37,66 @@ if not os.path.exists(LOG_DIR):
 
 def parse_args(argv):
     parser = ArgumentParser(description='train videos to image dataset')
-    parser.add_argument('--vid-root', type=str, help='path to videos')
-    parser.add_argument('--save-root', type=str, help='path to save images')
-    parser.add_argument('-f', '--val-factor', type=float, help='validation size factor', default=0.2)
-    parser.add_argument('-s', '--grab-step', type=int, help='grabbing images step', default=3)
-    parser.add_argument('-n', '--n-jobs', type=int, help='number of threads', default=4)
+    parser.add_argument('--vid-root',
+                        type=str,
+                        help='path to videos')
+    parser.add_argument('--save-root',
+                        type=str,
+                        help='path to save images')
+    parser.add_argument('-f', '--val-factor',
+                        type=float,
+                        default=0.2,
+                        help='validation size factor')
+    parser.add_argument('-s', '--grab-step',
+                        type=int,
+                        default=3,
+                        help='grabbing images step')
+    parser.add_argument('-n', '--n-jobs',
+                        type=int,
+                        default=4,
+                        help='number of threads')
+    parser.add_argument('-ff', '--face-bbox-expand-factor',
+                        type=tuple,
+                        default=(2, 2.5),
+                        help='expand face bounding box width and height by factor parameters')
     return parser.parse_args(argv)
 
 
-def crop_faces(root_video_dir, videos_list, save_img_dir, meta_data, step=3, name='0'):
+def _get_factorized_bbox(face_coords, bbox_factor):
+    """
+    scale face bounding box w, h by given factor
+    :param face_coords: tuple, face coordinates
+    :param bbox_factor: tuple, w and h scale factor
+    :return: scaled_top, scaled_right, scaled_bottom, scaled_left
+    """
+    global IMAGE_WIDTH, IMAGE_HEIGHT
+    top, right, bottom, left = face_coords
+    face_w = right - left
+    face_h = bottom - top
+    new_w = face_w * bbox_factor[0]
+    new_h = face_h * bbox_factor[1]
+    bias_w = int((new_w - face_w) // 2)
+    bias_h = int((new_h - face_h) // 2)
+    # to avoid going abroad image
+    top = max(0, top - bias_h)
+    left = max(0, left - bias_w)
+    bottom = min(IMAGE_HEIGHT, bottom + bias_h)
+    right = max(IMAGE_WIDTH, right + bias_w)
+    return top, right, bottom, left
+
+
+def crop_faces(root_video_dir, videos_list, save_img_dir, meta_data, step, bbox_factor, name='0'):
+    """
+    Pass through the given videos and grubs faces from each frame with  the given step
+    :param root_video_dir: directory contains video data
+    :param videos_list: list of videos names to process
+    :param save_img_dir: image saving directory
+    :param meta_data: file with labels
+    :param step: int, frames step to get faces,
+    :param bbox_factor: face bbox scaling factor
+    :param name: log file name
+    :return: None
+    """
     global VID_NUM_FRAMES
     log_path = name + '.log'
     logger = get_logger(log_path)
@@ -62,7 +115,7 @@ def crop_faces(root_video_dir, videos_list, save_img_dir, meta_data, step=3, nam
             if idx % step == 0:
                 faces = face_locations(im)
                 for fc_coords in faces:
-                    top, right, bottom, left = fc_coords
+                    top, right, bottom, left = _get_factorized_bbox(fc_coords, bbox_factor)
                     face = im[top:bottom, left:right]
                     face_path = img_prefix + "_" + str(idx) + '.jpg'
                     annotations[face_path] = labels[meta_data[vid]['label']]
@@ -82,6 +135,7 @@ if __name__ == "__main__":
     grab_step = args.grab_step
     root_dir = args.vid_root
     root_img_dir = args.save_root
+    face_factor = args.face_bbox_expand_factor
 
     # parse video directory
     # get videos
@@ -109,7 +163,7 @@ if __name__ == "__main__":
     print('spawning {} train handlers'.format(train_n_jobs))
     for i in range(train_n_jobs):
         process = Process(target=crop_faces, args=(root_dir, train_split[i], train_save_path, meta, grab_step,
-                                                   os.path.join(LOG_DIR, 'train_' + str(i))))
+                                                   face_factor, os.path.join(LOG_DIR, 'train_' + str(i))))
         processes.append(process)
         process.start()
 
@@ -121,7 +175,7 @@ if __name__ == "__main__":
     print('spawning {} validation handlers'.format(val_n_jobs))
     for i in range(val_n_jobs):
         process = Process(target=crop_faces, args=(root_dir, val_split[i], val_save_path, meta, grab_step,
-                                                   os.path.join(LOG_DIR, 'val_' + str(i))))
+                                                   face_factor, os.path.join(LOG_DIR, 'val_' + str(i))))
         processes.append(process)
         process.start()
 
